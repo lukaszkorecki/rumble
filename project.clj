3f2b855510c2c8a0e25fef2af416203dfa0702dc
Łukasz Korecki <lukasz@korecki.me>
2022-04-17 10:21:06 -0700

    Fix deployment config

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "https://github.com/lukaszkorecki/rumble"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
+  :deploy-repositories {"clojars" {:sign-releases false
+                                   :username :env/clojars_username
+                                   :password :env/clojars_password}}
+
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.namespace "1.2.0"]
                  [lambdaisland/kaocha "1.65.1029"]]
