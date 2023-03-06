0251b4238d2b25217f7cd4e19b20552927c1b43d
Łukasz Korecki <lukasz@korecki.me>
2023-03-06 15:13:23 -0800

    0.1.0-SNAPSHOT-5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-4"
+(defproject org.clojars.lukaszkorecki/rumble "0.1.0-SNAPSHOT-5"
   :description "(n)REPL helpers and tools"
   :url "https://github.com/lukaszkorecki/rumble"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
