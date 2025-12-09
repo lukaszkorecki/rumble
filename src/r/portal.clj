59b8ed422c5aa6d2bed74d671ff107a77e961f7d
Łukasz Korecki <lukasz@korecki.me>
2025-12-09 14:59:33 -0800

    Add github theme

--- src/r/portal.clj
+++ src/r/portal.clj
@@ -4,7 +4,7 @@
    [clojure.tools.namespace.repl :as ns.repl]
    [portal.browse.cljfx] ;; register cljfx launcher
    [portal.api :as portal]
-   [portal.colors]))
+   [portal.colors :as pc]))
 
 (set! *warn-on-reflection* true)
 
@@ -38,7 +38,26 @@
   (swap! tap-log conj msg)
   (portal.api/submit msg))
 
-;;; Experimental stuff
+(defn foo [] (println "r.portal loaded"))
+
+(pc/register! ::pc/github
+              {::pc/text "#24292f" ; Standard text color
+               ::pc/background "#ffffff" ; Main background (white)
+               ::pc/background2 "#f6f8fa" ; Secondary background (lighter gray)
+               ::pc/boolean "#0550ae" ; Blue for booleans
+               ::pc/string "#0a3069" ; Dark blue for strings
+               ::pc/keyword "#8250df" ; Purple for keywords
+               ::pc/namespace "#0550ae" ; Blue for namespaces
+               ::pc/tag "#116329" ; Green for tags
+               ::pc/symbol "#24292f" ; Standard text for symbols
+               ::pc/number "#0550ae" ; Blue for numbers
+               ::pc/uri "#0969da" ; Link blue for URIs
+               ::pc/border "#d0d7de" ; Border gray
+               ::pc/package "#0969da" ; Link blue for packages
+               ::pc/exception "#cf222e" ; Red for exceptions
+               ::pc/diff-add "#1a7f37" ; Green for additions
+               ::pc/diff-remove "#cf222e"} ; Red for removals
+              )
 
 (defn start!
   "Start portal instance and optionally open it in a browser"
@@ -50,7 +69,7 @@
    (let [a-portal (portal.api/open (merge
                                     (dissoc opts :browse?)
                                     {:window-title "Portal"
-                                     :theme :portal.colors/nord-light
+                                     :theme ::pc/github
                                      :launcher launcher}))
          url (portal.api/url a-portal)]
      (reset! instance a-portal)
